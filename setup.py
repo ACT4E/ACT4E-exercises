248ad970377547b2373f08e5755eca557d61c422
Andrea Censi <acensi@ethz.ch>
2021-02-20 13:24:11 +0100

    fix

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(name='ACT4E-exercises',
-      version="1.2",
+      version="1.2.2",
       package_dir={'': 'src'},
       packages=find_packages('src'),
       extras_require={},
-      )
+      zip_safe=False,
+include_package_data= True
+)
