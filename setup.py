ebe223ff740d704c024aba16a2e91d906b6c020e
Andrea Censi <acensi@ethz.ch>
2021-02-20 12:37:52 +0100

    fix

--- setup.py
+++ setup.py
@@ -4,7 +4,5 @@ setup(name='ACT4E-exercises',
       version="0.1",
       package_dir={'': 'src'},
       packages=find_packages('src'),
-
-      entry_points={"console_scripts": ["act4e-test = act4e_tests:test_main", ]},
       extras_require={},
       )
