736ad0b20b2d4958317f2d80acca0dc736960d43
Andrea Censi <acensi@ethz.ch>
2021-10-09 13:24:13 +0200

    refactoring of exercises

---
+++ src/act4e_interfaces_tests/loading.py
@@ -0,0 +1,26 @@
+import os
+from typing import List
+
+
+def get_data_dir() -> str:
+    from act4e_interfaces_tests import __file__
+
+    d = os.path.join(os.path.dirname(__file__), "data")
+    return d
+
+
+def list_data_files() -> List[str]:
+    d = get_data_dir()
+    return [f for f in os.listdir(d) if f.endswith(".yaml")]
+
+
+def load_test_data_file(fn: str) -> str:
+    d = get_data_dir()
+
+    fn2 = os.path.join(d, fn)
+    if not os.path.exists(fn2):
+        msg = f"Cannot find filename: {fn2}"
+        raise ValueError(msg)
+
+    with open(fn2) as f:
+        return f.read()
